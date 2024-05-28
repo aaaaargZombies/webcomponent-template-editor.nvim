5521e3795900609f1b334553f38cf8c565a4aa47
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-05-28 10:58:03 +0100

    tidyup

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,5 +1,3 @@
--- P('webcomponent template editor - runs at startup')
-
 local wc = require('webcomponent-template-editor')
 
 vim.api.nvim_create_user_command('TEST', wc.print_templates, { range = true })
