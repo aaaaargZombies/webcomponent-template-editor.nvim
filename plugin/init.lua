bf9171e5c109397565579a012ca7381aa256f429
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-05-28 10:58:34 +0100

    give command a more sensible name

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,3 +1,3 @@
 local wc = require('webcomponent-template-editor')
 
-vim.api.nvim_create_user_command('TEST', wc.print_templates, { range = true })
+vim.api.nvim_create_user_command('WCEdit', wc.print_templates, { range = true })
